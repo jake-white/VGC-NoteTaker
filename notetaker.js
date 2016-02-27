ba8eb7df49208b3f08e7790f7951f1f85b1a8216
Jake White <squirrelboy1225@gmail.com>
2016-02-27 00:46:12 -0500

    added stage boosts/drops, fixed battle clearing

--- notetaker.js
+++ notetaker.js
@@ -1,30 +1,44 @@
 var restrictedList, megaList, otherList, rawPokemonList;
 var previews = document.getElementsByClassName("preview");
+var atk, def, spatk, spdef, spe, acc, eva, valueMem;
 var previewColors = document.getElementsByClassName("previewcolor");
+var statElements = document.getElementsByClassName("statElements");
+var statConversion;
+var pkdexArray;
 var c;
 
-$(document).ready(function(){
+$(document).ready(function(){ //called when jquery determines the page is ready
+	atk = [0,0,0,0,0,0,0];
+	def = [0,0,0,0,0,0,0];
+	spatk = [0,0,0,0,0,0,0];
+	spdef = [0,0,0,0,0,0,0];
+	spe = [0,0,0,0,0,0,0];
+	acc = [0,0,0,0,0,0,0];
+	eva = [0,0,0,0,0,0,0];
+	setStatuses();
+	pkdexArray = JSONtoArray(POKEDEX_XY);
 	c = new Array(previews.length);
-	var pkdexArray = JSONtoArray(POKEDEX_XY);
+	valueMem = new Array(previews.length);
 	for(var i = 0; i < previews.length; ++i)
 	{
-		previews[i].innerHTML += `<select>
+		valueMem[i] = ["Attack = 0"];
+		previews[i].innerHTML = `<select>
 									<option>Not Seen</option>
 									<option>Brought</option>
-								</select>`
-		previews[i].innerHTML += `<select>
+								</select>
+								<select>
 									<option>Healthy</option>
 									<option>Burned</option>
 									<option>Paralyzed</option>
 									<option>Poisoned</option>
 									<option>Fainted</option>
-								</select>`
+								</select>` + previews[i].innerHTML;
 		c[i] = completely(previews[i]);
 		c[i].options = pkdexArray;
 	}
 });
 
-var JSONtoArray = function(JSONThing){
+var JSONtoArray = function(JSONThing){ //converts JSON to array of things
 	var array = new Array();
 	var id = 0;
 	for(var thing in JSONThing){
@@ -35,7 +49,7 @@ var JSONtoArray = function(JSONThing){
 	return array;
 }
 
-var auto = function(preset){
+var auto = function(preset){ //auto select a team
 	var presetArray = new Array(6);
 	if(preset=="bigsix"){
 		presetArray = ["Groudon", "Xerneas", "Kangaskhan", "Salamence", "Smeargle", "Talonflame"];
@@ -52,16 +66,8 @@ var auto = function(preset){
 	analyze();
 }
 
-var bring = function(number){
-	previewColors[number].style.background = color;
-}
-var faint = function(number){
-	previewColors[number].style.background = "#ff4c4c";
-}
-
-var effects = function(id, turn){
+var effects = function(id, turn){ //editing a field effect
 	var elements = document.getElementsByClassName(id);
-	console.log(elements[0].style);
 	for(var i = 0; i < elements.length; ++i){
 		if(i < turn)
 			elements[i].style.background = '#43ABA4';
@@ -71,9 +77,74 @@ var effects = function(id, turn){
 	}
 }
 
-var clearBattle = function(){
-	console.log("CLEARED")
+var clearBattle = function(){ //clearing effects, statuses, pokemon, etc
+	effects('tw_player', 0);
+	effects('tw_opp', 0);
+	effects('tr', 0);
+	atk = [0,0,0,0,0,0,0];
+	def = [0,0,0,0,0,0,0];
+	spatk = [0,0,0,0,0,0,0];
+	spdef = [0,0,0,0,0,0,0];
+	spe = [0,0,0,0,0,0,0];
+	acc = [0,0,0,0,0,0,0];
+	eva = [0,0,0,0,0,0,0];
+	for(var i = 0; i < previews.length; ++i){
+		previews[i].innerHTML = '<div class = "statElements"></div>';
+	}
+	setStatuses();
+	for(var i = 0; i < previews.length; ++i){
+		previews[i].innerHTML = `<select>
+									<option>Not Seen</option>
+									<option>Brought</option>
+								</select>
+								<select>
+									<option>Healthy</option>
+									<option>Burned</option>
+									<option>Paralyzed</option>
+									<option>Poisoned</option>
+									<option>Fainted</option>
+								</select>` + previews[i].innerHTML;
+		c[i] = completely(previews[i]);
+		c[i].options = pkdexArray;
+	}
 	auto("clear"); //clearing team preview
+}
+
+var setStatuses = function(){ //updates all boosts/drops
+	for(var i = 0; i < previews.length; ++i){
+		statElements[i].innerHTML=`<select id='stat` + i + `'>
+									<option>Attack = ` + atk[i] + `</option>
+									<option>Defense = ` + def[i] + `</option>
+									<option>Special Attack = ` + spatk[i] + `</option>
+									<option>Special Defense = ` + spdef[i] + `</option>
+									<option>Speed = ` + spe[i] + `</option>
+									<option>Accuracy = ` + acc[i] + `</option>
+									<option>Evasion = ` + eva[i] + `</option>
+								</select>
+								<input type='button' onclick='statInc(`+i+`, 1);' value='+'>
+								<input type='button' onclick='statInc(`+i+`, -1);' value='-'>`
+		statConversion = {
+			"Attack": atk,
+			"Defense": def,
+			"Special Attack": spatk,
+			"Special Defense": spdef,
+			"Speed": spe,
+			"Accuracy": acc,
+			"Evasion": eva
+		}
+	}
+}
+
+var statInc = function(id, inc){
+	var originalValue = document.getElementById("stat"+id).value;
+	var value = originalValue.substring(0, originalValue.indexOf('=')-1);
+	if(statConversion[value][id]+inc <= 6 && statConversion[value][id]+inc >= -6)
+	statConversion[value][id]+=inc;
+	setStatuses();
+	valueMem[id] = originalValue.substring(0, originalValue.indexOf('=') + 1) + ' ' + statConversion[value][id]; //so we can remember what the selector was on last
+	for(var i = 0; i < valueMem.length; ++i){
+		document.getElementById("stat"+i).value = valueMem[i];
+	}
 }
 
 var analyze = function(){
